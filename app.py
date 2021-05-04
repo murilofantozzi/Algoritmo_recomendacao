e479aae50cfc9a04cf494e90d9d045e3afe39b83
Murilo <murilofantozzi87@gmail.com>
2021-05-03 23:22:30 -0300

    Definição dos dados

--- app.py
+++ app.py
@@ -1,2 +1,8 @@
 import numpy as np
 import pandas as pd
+
+dados1 = pd.read_csv("dados_dia1.csv") 
+dados2 = pd.read_csv("dados_dia2.csv") 
+dados3 = pd.read_csv("dados_dia3.csv") 
+
+print (dados1)
